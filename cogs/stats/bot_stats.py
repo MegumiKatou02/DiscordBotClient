45dc15766cb1363bf9daf027fad31eba01564e6c
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-11-23 19:22:36 +0700

    config hex color

--- cogs/stats/bot_stats.py
+++ cogs/stats/bot_stats.py
@@ -31,7 +31,7 @@ class BotStats(commands.Cog):
 
         version_bot = setup_bot.VERSION
 
-        embed = discord.Embed(title=f"Thông tin bot: {bot_name}", color=discord.Color.blue())
+        embed = discord.Embed(title=f"Thông tin bot: {bot_name}", color=0x67ff4f)
         embed.set_thumbnail(url=bot_avatar)
         embed.add_field(name="Phiên bản", value=version_bot, inline=False)
         embed.add_field(name="Số lượng server", value=guild_count, inline=False)
