c25f6c51da1b51a8d5bd520fb50254cbf4beefa7
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-11-25 19:11:20 +0700

    config event manage

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ async def load_cogs():
     await clients.load_extension("cogs.voice")
     await clients.load_extension("cogs.stats.server_stats")
     await clients.load_extension("cogs.stats.bot_stats")
+    await clients.load_extension("cogs.administration.event")
     
 #goodbye
 @clients.tree.command()
