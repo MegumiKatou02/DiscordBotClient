dd06ecf010294531e6f54a16bed0ae4382691890
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-01-10 11:04:46 +0700

    Update bot owner display name and adjust member/channel field layout for better readability

--- cogs/stats/bot_stats.py
+++ cogs/stats/bot_stats.py
@@ -31,7 +31,7 @@ class BotStats(commands.Cog):
 
         bot_owner_id = config.USER_ID
         bot_owner = await interaction.guild.fetch_member(bot_owner_id)
-        bot_owner_name = bot_owner.name if bot_owner else "Không tìm thấy"
+        bot_owner_name = bot_owner.name if bot_owner else "Yukiookii"
 
         version_bot = self.data["version"]
 
@@ -39,8 +39,8 @@ class BotStats(commands.Cog):
         embed.set_thumbnail(url=bot_avatar)
         embed.add_field(name="Phiên bản", value=version_bot, inline=False)
         embed.add_field(name="Số lượng server", value=guild_count, inline=False)
-        embed.add_field(name="Số lượng thành viên", value=member_count, inline=False)
-        embed.add_field(name="Số lượng kênh", value=channel_count, inline=False)
+        embed.add_field(name="Số lượng thành viên", value=member_count, inline=True)
+        embed.add_field(name="Số lượng kênh", value=channel_count, inline=True)
         embed.add_field(name="Thời gian hoạt động", value=combined_uptime, inline=False)
         embed.add_field(name="Chủ sở hữu bot", value=bot_owner_name, inline=False)
 
